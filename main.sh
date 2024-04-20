55d2a77560fd5aba67c891436ed11dd9569548ff
bradyt005 <bthompson17@luc.edu>
2024-04-19 21:18:20 -0500

    done

--- main.sh
+++ main.sh
@@ -1,2 +1,42 @@
+#!/bin/bash
 
-hello
+mkdir -p output
+
+mv tentext output/
+
+cd output
+
+cat tentext > read.txt
+
+pwd > pwd.txt
+
+ls > ls.txt
+
+cp tentext copy.txt
+
+date > date.txt
+
+wc -w tentext > textcount.txt
+
+ps > process_temp.txt
+head -n 5 process_temp.txt > process.txt
+rm process_temp.txt
+
+ifconfig > net_temp.txt
+
+head -n 5 net_temp.txt > netstat.txt
+rm net_temp.txt
+
+mount > mount_temp.txt
+head -n 5 mount_temp.txt > mount.txt
+rm mount_temp.txt
+
+touch permissions.txt
+chmod 777 permissions.txt
+
+TESTENV1="test"
+export TESTENV1
+
+grep -o '\b\w\{3,\}\b' tentext > regex.txt
+
+cd ..
